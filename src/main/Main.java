fc4e60b32164bb5a62ecd17d45244eb5c4a7e9d6
Ganesh Patil <144130668+Ganesh-D-patil@users.noreply.github.com>
2026-07-23 18:04:10 +0530

    updated main file

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,8 @@ package main;
 
 import model.Account;
 import model.Customer;
+import model.Transaction;
+import model.Admin;
 
 public class Main {
 
@@ -27,6 +29,24 @@ public class Main {
                 50000,
                 "Active"
         );
+        Transaction transaction = new Transaction(
+        		1,
+        		788750927,
+        		"Deposite",
+        		2234,
+        		"23-07-26",
+        		"5:50 AM",
+        		"Success"
+        		);
+        
+        Admin admin = new Admin(
+                1,
+                "Ganesh Patil",
+                "MayurGhuge@gmail.com",
+                "Pass123"
+        );
+
+        System.out.println(admin);
 
         // Print Customer Details
         System.out.println(customer);
@@ -36,5 +56,9 @@ public class Main {
 
         // Print Account Details
         System.out.println(account);
+        
+        System.out.println();
+        
+        System.out.println(transaction);
     }
 }
